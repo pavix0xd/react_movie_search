5051691051212d5cf55442ab9ccf10c7e837ac37
pavix0xd <minindupavith20@gmail.com>
2025-05-21 16:48:09 +0530

    Refactor project structure: move CSS files to a dedicated styles directory, add NavBar component, and update App component to include NavBar and new styles.

--- movie_search/src/App.jsx
+++ movie_search/src/App.jsx
@@ -1,16 +1,20 @@
-import './App.css'
+import './styles/App.css'
 import Home from './pages/Home'
 import Favorites from './pages/Favorites'
 import {Routes, Route} from 'react-router-dom'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
+    <div>
+      <NavBar/>
     <main className="main-content">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </main>
+    </div>
   )
 }
 
