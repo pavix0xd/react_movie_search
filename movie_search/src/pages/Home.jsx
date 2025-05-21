d07a9c4ee5170aebf54a2822cb22c29f8c1f2fcc
pavix0xd <minindupavith20@gmail.com>
2025-05-21 16:22:50 +0530

    Refactor Home component to implement search functionality and improve form structure

--- movie_search/src/pages/Home.jsx
+++ movie_search/src/pages/Home.jsx
@@ -1,29 +1,43 @@
 import React from "react";
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 const Home = () => {
+
+    const [searchQuery, setSearchQuery] = useState("");
+
+
   const movies = [
     { id: 1, title: "John Wick", release_date: "2020" },
     { id: 2, title: "Terminator", release_date: "1999" },
     { id: 3, title: "The Matrix", release_date: "1998" },
   ];
 
-const handleSearch = () => {
-
-}
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(`Searching for: ${searchQuery}`);
+    setSearchQuery("");
+  };
 
   return (
     <div className="home">
-
-        <Form onSubmit = {handleSearch} className="search-form">
-            <input type="text" placeholder="Search for a movie..." />
-            <button type="submit" className="search-btn">Search</button>
-        </Form>
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for a movie..."
+          className="search-input"
+          value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-btn">
+          Search
+        </button>
+      </form>
 
       <div className="movies-grid">
-        {movies.map((movie) => (
-          <MovieCard movie={movie} key={movie.id} />
-        ))}
+        {movies.map((movie) => 
+            <MovieCard movie={movie} key={movie.id} />
+        )}
       </div>
     </div>
   );
